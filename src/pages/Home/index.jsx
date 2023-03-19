7cf85269c5fe099ee0fe876e51f3ba3c3c95cecd
Muhammad Elzandi Yusuf <elzandimilanisty29@gmail.com>
2023-03-19 23:32:13 +0700

    feat(api): integrate api location

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,87 @@
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { AiOutlinePlus, AiOutlineSearch } from 'react-icons/ai';
+import Localbase from 'localbase';
 
 import Layout from '@/components/Layout';
 import Header from '@/components/Header';
 import Textfield from '@/components/Textfield';
 import Button from '@/components/Button';
+import { apiService } from '@/utils/api/actionGeneralApi';
+import { GetDataArea, GetDataProduct, GetDataSize } from '@/utils/api/methodConstApi';
 
 const Home = () => {
+  const localDb = new Localbase('efishery');
+  const getList = async () => {
+    const listProduct = await apiService(GetDataProduct);
+    console.log('listProduct', listProduct);
+  };
+  const getListOfLocation = async () => {
+    const listOfProvince = [];
+    const listOfCity = [];
+
+    localDb
+      .collection('province')
+      .get()
+      .then(async (collections) => {
+        if (collections.length === 0) {
+          const resData = await apiService(GetDataArea);
+          if (resData && resData.length > 0) {
+            resData.forEach((element) => {
+              const checkProvince = listOfProvince.find((row) => row.label === element.province);
+              if (!checkProvince) {
+                const obj = {};
+                obj.value = element.province;
+                obj.label = element.province;
+                listOfProvince.push(obj);
+              }
+              const checkDoubleCity = listOfCity.find((row) => row.label === element.city);
+              if (!checkDoubleCity) {
+                const obj = {};
+                obj.value = element.city;
+                obj.label = element.city;
+                obj.province = element.province;
+                listOfCity.push(obj);
+              }
+            });
+
+            localDb.collection('province').add({ data: listOfProvince });
+            localDb.collection('city').add({ data: listOfCity });
+          }
+        }
+      });
+  };
+
+  const getListOfSize = async () => {
+    const listOfSize = [];
+
+    localDb
+      .collection('driver')
+      .get()
+      .then(async (collections) => {
+        if (collections.length === 0) {
+          const resData = await apiService(GetDataSize);
+
+          if (resData && resData.length > 0) {
+            resData.forEach((element) => {
+              const obj = {};
+              obj.value = element.size;
+              obj.label = element.size;
+              listOfSize.push(obj);
+            });
+
+            localDb.collection('size').add({ data: listOfSize });
+          }
+        }
+      });
+  };
+
+  useEffect(() => {
+    getList();
+    getListOfLocation();
+    getListOfSize();
+  }, []);
+
   return (
     <Layout>
       <Helmet>Daftar Harga - eFishery</Helmet>
